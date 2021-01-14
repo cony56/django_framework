608c700edc2666168c17c5e53a82685fc1688900
cony56 <taeyup56@hanmail.net>
2021-01-14 14:17:52 +0900

    '20210113'

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from django import forms
+
+def min_length_3_validator(value):
+    if len(value)<3:
+        raise forms.ValidationError('글제목은 3글자 이상 입력해주세요.')
 
 class Post(models.Model):
 
@@ -7,7 +12,7 @@ class Post(models.Model):
     # 작성자
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     # 글제목
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, validators=[min_length_3_validator])
     # 글내용
     text = models.TextField()
     # 작성일
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,102 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post
 # Post 목록
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+from .models import Post
+from .forms import PostModelForm, PostForm
+
+# Post 삭제
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+# Post 수정 : Model Form 사용
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostModelForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            # 작성자
+            post.author = User.objects.get(username=request.user.username)
+            # 글 게시날짜
+            post.published_date = timezone.now()
+            # 실제 갱신됨
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostModelForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form':form})
+
+
+# Post 등록 : Form 사용
+
+def post_new_form(request):
+    if request.method =='POST':
+        form = PostForm(request.POST)
+        # POST 요청일 때
+        if form.is_valid():
+            # form 데이터가 clean 한 상태
+            print(form.cleaned_data)
+            post = Post.objects.create(author=User.objects.get(username=request.user.username),
+                                title=form.cleaned_data['title'],
+                                text=form.cleaned_data['text'],
+                                published_date =timezone.now())
+            return redirect('post_detail', pk=post.pk)
+
+
+    else:
+        # 등록하는 빈 폼을 보여주기
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+# Post 등록 : ModelForm 사용
+@login_required
+
+
+def post_new(request):
+    if request.method =='POST':
+        # 실제 등록 처리하기
+        form = PostModelForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            # 작성자
+            post.author = User.objects.get(username=request.user.username)
+            # 글 게시날짜
+            post.published_date = timezone.now()
+            # 실제 등록됨
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        #등록하는 빈 폼을 보여주기
+        form = PostModelForm()
+    return render(request, 'blog/post_edit.html',{'form': form})
+
+# Post 상세정보
+def post_detail(request, pk):
+    #db에서 읽어온 것
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
+
+
+
+# Post목록
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_list_response(request):
     name ='Django'
@@ -15,9 +106,3 @@ def post_list_response(request):
     response.write(f'<p>HTTP ContentType : {request.content_type}</p>')
     #return HttpResponse(f'''<h2>Hello {name}!!</h2><p>HTTP METHOD : {request.method}</p>''')
     return response
-
-# Post목록
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
-
